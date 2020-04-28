80bc142add176cf00ee807b1a43f44332bc9c2f9
skolatv <64169007+skolatv@users.noreply.github.com>
2020-04-28 22:33:04 -0100

    Add files via upload
    center login page.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "36226741fb1c82d04747b31112745ebe",
 "/": "36226741fb1c82d04747b31112745ebe",
-"main.dart.js": "1bfa48171272fc20379faa2952c3ce33",
+"main.dart.js": "dedb4cc2b12b527f75fef647257747e9",
 "manifest.json": "54b18457669bfe44027ce86a62568d60"
 };
 
